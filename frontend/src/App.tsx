684169463fd175b4f817a9a38c1a7089ad6a6fcd
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-10-03 23:23:57 +0100

    navbar completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,9 @@
 import React from 'react';
 import './App.css';
+import { NavBar } from './layouts/home/components/NavBar';
 
-function App() {
+export const App = () => {
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <div className='d-flex mx-auto'>
-          <a className="navbar-brand me-5" href="#">
-            <img src={require('./logos/logo-no-background.png')} width={220} alt="book"/>
-          </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav mx-5">
-              <li className="nav-item">
-                <a className="nav-link text-white active" aria-current="page" href="#">Sport Cars</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#">Best Sellers</a>
-              </li>
-            </ul>
-            <div className='d-flex align-items-center ms-5'>
-              <div className='d-flex align-items-center me-3'>
-                <i className="bi bi-star-fill" style={{color: '#83888f'}}></i>
-                <a className="nav-link text-white ms-2" href="#">Favorites</a>
-              </div>
-              <button type="button" className="btn btn-primary">Login</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <NavBar/>
   );
 }
-
-export default App;
---
+++ frontend/src/layouts/home/components/NavBar.tsx
@@ -0,0 +1,27 @@
+export const NavBar = () => {
+
+    return (
+        <nav className="navbar navbar-dark">
+            <div className="container-fluid flex-column flex-md-row justify-content-center justify-content-sm-between mx-sm-5 my-sm-2">
+                <a className="navbar-brand mb-2 mb-md-0">
+                    <img src={require('../../../logos/logo-no-background.png')} width={180} alt="logo"/>
+                </a>
+                <div className='d-none d-lg-flex'>
+                    <div className="input-group input-group-sm">
+                        <input type="text" className="form-control shadow-none border" placeholder="Any make, model..." aria-label="Any make, model..." aria-describedby="home-search"/>
+                        <button className="btn btn-outline-secondary" type="button" id="home-search">
+                            <i className="bi bi-search"></i>
+                        </button>
+                    </div>
+                </div>
+                <div className='d-flex align-items-center'>
+                <a className='d-flex nav-link me-4' href="#">
+                    <i className="bi bi-star-fill" style={{color: '#83888f'}}></i>
+                    <span className='ms-2 text-white'>Favorites</span>
+                </a>
+                    <button type="button" className="btn btn-primary btn-sm px-5">Login</button>
+                </div>
+            </div>
+        </nav>
+    );
+}
