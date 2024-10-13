28869bd539c3e5ed93a388b2fccd023b7875213a
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-10-13 21:04:28 +0100

    car card complete

--- frontend/src/layouts/search/components/CarCard/CarCard.tsx
+++ frontend/src/layouts/search/components/CarCard/CarCard.tsx
@@ -2,17 +2,46 @@ import './CarCard.css'
 
 export const CardCard = () => {
 
+    const formatValueSpaces = (value: number) => {
+        return new Intl.NumberFormat('en-US', {
+            style: 'decimal',
+            maximumFractionDigits: 0,
+        })
+            .format(value)
+            .replace(/,/g, ' ');
+    };
+
     return (
-        <div className="card w-100" style={{backgroundColor: 'var(--color-background-dark-contrast)'}}>
+        <div className="card w-100" style={{ backgroundColor: 'var(--color-background-dark-contrast)' }}>
             <div className="row g-0">
                 <div className="col-md-3 card-image">
                     <img src={require('../../../../images/cars/recommendation-mercedes-E220.jpg')} className="img-fluid rounded-start" alt="Car" />
                 </div>
                 <div className="col-md-9">
-                    <div className="card-body">
-                        <h5 className="card-title text-white">Mercedes-Benz E 220 d AMG Line</h5>
-                        <p className="card-text text-white">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                    <div className="card-body d-flex flex-row justify-content-between align-items-center h-100">
+                        <div className='h-100'>
+                            <h5 className="card-title text-white">Mercedes-Benz E 220 d AMG Line</h5>
+                            <p className="card-text text-white">{formatValueSpaces(1950)} cm3 • 194 hp</p>
+                            <div className='d-flex flex-row flex-wrap' style={{ columnGap: '20px' }}>
+                                <div className='d-flex flex-row align-items-center car-card-characteristic'>
+                                    <i className="bi bi-speedometer2" style={{ color: 'white' }}></i>
+                                    <p className='text-white'>{formatValueSpaces(189998)} KM</p>
+                                </div>
+                                <div className='d-flex flex-row align-items-center car-card-characteristic'>
+                                    <i className="bi bi-fuel-pump" style={{ color: 'white' }}></i>
+                                    <p className='text-white'>Diesel</p>
+                                </div>
+                                <div className='d-flex flex-row align-items-center car-card-characteristic'>
+                                    <i className="bi bi-car-front" style={{ color: 'white' }}></i>
+                                    <p className='text-white'>Automatic</p>
+                                </div>
+                                <div className='d-flex flex-row align-items-center car-card-characteristic'>
+                                    <i className="bi bi-calendar-event" style={{ color: 'white' }}></i>
+                                    <p className='text-white'>2019</p>
+                                </div>
+                            </div>
+                        </div>
+                        <span className='card-text fs-4' style={{ color: 'var(--color-main-orange)' }}>{formatValueSpaces(37500)} €</span>
                     </div>
                 </div>
             </div>
