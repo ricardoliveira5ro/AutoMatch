38a48b7c6dfc0f3eabd8c24920819eba22fe6fbb
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-10-14 16:10:06 +0100

    car details page (starting)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Login } from './layouts/authentication/Login/Login';
 import { Register } from './layouts/authentication/Register/Register';
 import { Search } from './layouts/search/Search';
+import { CarDetails } from './layouts/carDetails/CarDetails';
 
 export const App = () => {
   return (
@@ -14,6 +15,7 @@ export const App = () => {
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/search' element={<Search/>}/>
+        <Route path='/cars/:id' element={<CarDetails/>}/>
       </Routes>
     </div>
   );
