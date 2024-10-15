e5de6246b89c2c8e37bc041a8a070d57184a7521
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-10-15 14:14:45 +0100

    car key info

--- frontend/src/layouts/carDetails/CarDetails.tsx
+++ frontend/src/layouts/carDetails/CarDetails.tsx
@@ -7,11 +7,60 @@ export const CarDetails = () => {
 
     const { id } = useParams();
 
+    const formatValueSpaces = (value: number) => {
+        return new Intl.NumberFormat('en-US', {
+            style: 'decimal',
+            maximumFractionDigits: 0,
+        })
+            .format(value)
+            .replace(/,/g, ' ');
+    };
+
     return (
         <div className='container w-100'>
             <div className='row g-0 py-3'>
-                <Carousel/>
-                <KeyDetails/>
+                <Carousel />
+                <KeyDetails formatValueSpaces={formatValueSpaces} />
+            </div>
+            <div className='d-flex flex-row flex-wrap justify-content-evenly align-items-center car-key-info-container'>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <i className="bi bi-speedometer2 lh-1" style={{ color: 'white' }}></i>
+                        <p className='text-white mb-2 fs-6'>Mileage</p>
+                        <span className='text-white fs-5'>{formatValueSpaces(189998)} KM</span>
+                    </div>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <i className="bi bi-fuel-pump lh-1" style={{ color: 'white' }}></i>
+                        <p className='text-white mb-2 fs-6'>Fuel</p>
+                        <span className='text-white fs-5'>Diesel</span>
+                    </div>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <i className="bi bi-gear lh-1" style={{ color: 'white' }}></i>
+                        <p className='text-white mb-2 fs-6'>Gear Box</p>
+                        <span className='text-white fs-5'>Automatic</span>
+                    </div>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <i className="bi bi-calendar-event lh-1" style={{ color: 'white' }}></i>
+                        <p className='text-white mb-2 fs-6'>Year</p>
+                        <span className='text-white fs-5'>2019</span>
+                    </div>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <i className="bi bi-ev-front lh-1" style={{ color: 'white' }}></i>
+                        <p className='text-white mb-2 fs-6'>Horse Power</p>
+                        <span className='text-white fs-5'>194 HP</span>
+                    </div>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <i className="bi bi-box-seam-fill lh-1" style={{ color: 'white' }}></i>
+                        <p className='text-white mb-2 fs-6'>Displacement</p>
+                        <span className='text-white fs-5'>{formatValueSpaces(1950)} cm3</span>
+                    </div>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <i className="bi bi-car-front lh-1" style={{ color: 'white' }}></i>
+                        <p className='text-white mb-2 fs-6'>Style</p>
+                        <span className='text-white fs-5'>Coupe</span>
+                    </div>
+                </div>
+            <div className='row g-0 py-3'>
+                
             </div>
         </div>
     );
--- frontend/src/layouts/carDetails/KeyDetails/KeyDetails.tsx
+++ frontend/src/layouts/carDetails/KeyDetails/KeyDetails.tsx
@@ -1,23 +1,17 @@
+import React from 'react';
 import './KeyDetails.css'
 
-export const KeyDetails = () => {
-
-    const formatValueSpaces = (value: number) => {
-        return new Intl.NumberFormat('en-US', {
-            style: 'decimal',
-            maximumFractionDigits: 0,
-        })
-            .format(value)
-            .replace(/,/g, ' ');
-    };
+export const KeyDetails: React.FC<{
+    formatValueSpaces: any
+}> = (props) => {
 
     return (
-        <div className='col-5 px-4'>
+        <div className='col-5 ps-4'>
             <div className='car-key-details-container d-flex flex-column justify-content-between p-3' style={{ height: '55vh' }}>
                 <div>
                     <span className='fs-5'>Mercedes-Benz E 220 d AMG Line</span>
                     <p className='fs-6'>Class E 220</p>
-                    <span className='fs-4' style={{ color: 'var(--color-main-orange)' }}>{formatValueSpaces(37500)} €</span>
+                    <span className='fs-4' style={{ color: 'var(--color-main-orange)' }}>{props.formatValueSpaces(37500)} €</span>
                     <hr className='text-white'></hr>
                     <span>Ricardo Oliveira</span>
                     <p>Lisbon</p>
