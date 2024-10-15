560564d62614ba486326c37507ed1fc82219d966
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-10-15 22:48:10 +0100

    CarKeyInfo

--- frontend/src/layouts/carDetails/CarDetails.tsx
+++ frontend/src/layouts/carDetails/CarDetails.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
-import { Carousel } from './Carousel/Carousel';
-import { KeyDetails } from './KeyDetails/KeyDetails';
+import { Carousel } from './components/Carousel/Carousel';
+import { KeyDetails } from './components/KeyDetails/KeyDetails';
+import { CarKeyInfo } from './components/CarKeyInfo/CarKeyInfo';
 import './CarDetails.css'
 
 export const CarDetails = () => {
@@ -22,45 +23,9 @@ export const CarDetails = () => {
                 <Carousel />
                 <KeyDetails formatValueSpaces={formatValueSpaces} />
             </div>
-            <div className='d-flex flex-row flex-wrap justify-content-evenly align-items-center car-key-info-container'>
-                    <div className='d-flex flex-column justify-content-center align-items-center'>
-                        <i className="bi bi-speedometer2 lh-1" style={{ color: 'white' }}></i>
-                        <p className='text-white mb-2 fs-6'>Mileage</p>
-                        <span className='text-white fs-5'>{formatValueSpaces(189998)} KM</span>
-                    </div>
-                    <div className='d-flex flex-column justify-content-center align-items-center'>
-                        <i className="bi bi-fuel-pump lh-1" style={{ color: 'white' }}></i>
-                        <p className='text-white mb-2 fs-6'>Fuel</p>
-                        <span className='text-white fs-5'>Diesel</span>
-                    </div>
-                    <div className='d-flex flex-column justify-content-center align-items-center'>
-                        <i className="bi bi-gear lh-1" style={{ color: 'white' }}></i>
-                        <p className='text-white mb-2 fs-6'>Gear Box</p>
-                        <span className='text-white fs-5'>Automatic</span>
-                    </div>
-                    <div className='d-flex flex-column justify-content-center align-items-center'>
-                        <i className="bi bi-calendar-event lh-1" style={{ color: 'white' }}></i>
-                        <p className='text-white mb-2 fs-6'>Year</p>
-                        <span className='text-white fs-5'>2019</span>
-                    </div>
-                    <div className='d-flex flex-column justify-content-center align-items-center'>
-                        <i className="bi bi-ev-front lh-1" style={{ color: 'white' }}></i>
-                        <p className='text-white mb-2 fs-6'>Horse Power</p>
-                        <span className='text-white fs-5'>194 HP</span>
-                    </div>
-                    <div className='d-flex flex-column justify-content-center align-items-center'>
-                        <i className="bi bi-box-seam-fill lh-1" style={{ color: 'white' }}></i>
-                        <p className='text-white mb-2 fs-6'>Displacement</p>
-                        <span className='text-white fs-5'>{formatValueSpaces(1950)} cm3</span>
-                    </div>
-                    <div className='d-flex flex-column justify-content-center align-items-center'>
-                        <i className="bi bi-car-front lh-1" style={{ color: 'white' }}></i>
-                        <p className='text-white mb-2 fs-6'>Style</p>
-                        <span className='text-white fs-5'>Coupe</span>
-                    </div>
-                </div>
+            <CarKeyInfo formatValueSpaces={formatValueSpaces}/>
             <div className='row g-0 py-3'>
-                
+
             </div>
         </div>
     );
