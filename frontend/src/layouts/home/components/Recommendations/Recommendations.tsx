046aaa75b173b96d098e8d48dab2af89da91ee8a
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-10-18 22:11:39 +0100

    display list of cars from backend

--- frontend/src/layouts/home/components/Recommendations/Recommendations.tsx
+++ frontend/src/layouts/home/components/Recommendations/Recommendations.tsx
@@ -11,11 +11,9 @@ export const Recommendations = () => {
             style: 'decimal',
             maximumFractionDigits: 0,
         })
-        .format(price)
-        .replace(/,/g, ' ');
+            .format(price)
+            .replace(/,/g, ' ');
     };
-
-    const carId = 1;
 
     const [cars, setCars] = useState<CarModel[]>([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -24,8 +22,8 @@ export const Recommendations = () => {
     useEffect(() => {
         const fetchCars = async () => {
             const baseUrl: string = "http://localhost:8080/api/cars/";
-            const url: string = `${baseUrl}?page=0&size=10`
-    
+            const url: string = `${baseUrl}?page=0&size=12`
+
             const response = await fetch(url);
 
             if (!response.ok) {
@@ -43,6 +41,7 @@ export const Recommendations = () => {
                     condition: responseData[key].condition,
                     price: responseData[key].price,
                     style: responseData[key].style,
+                    date: responseData[key].date,
                     mileage: responseData[key].mileage,
                     fuelType: responseData[key].fuelType,
                     gearBox: responseData[key].gearBox,
@@ -52,7 +51,7 @@ export const Recommendations = () => {
                     horsePower: responseData[key].horsePower
                 });
             }
-        
+
             setCars(loadedCars);
             setIsLoading(false);
         };
@@ -65,7 +64,7 @@ export const Recommendations = () => {
 
     if (isLoading) {
         return (
-            <SpinnerLoading/>
+            <SpinnerLoading />
         )
     }
 
@@ -81,37 +80,19 @@ export const Recommendations = () => {
         <div className='container'>
             <div className='card p-4 recommendations-container'>
                 <h2 className='text-white text-center'>Recommendations</h2>
-                <div className='d-flex flex-wrap justify-content-center align-items-center mt-4' style={{columnGap: '30px', rowGap: '30px'}}>
-                    <Link to={`/cars/${carId}`} className="card card-recommendation">
-                        <img src={require('../../../../images/cars/recommendation-mercedes-E220.jpg')} className="card-img-top img-fluid" alt="Car Image"/>
+                <div className='d-flex flex-wrap justify-content-center align-items-center mt-4' style={{ columnGap: '30px', rowGap: '30px' }}>
+                    {cars.map(car => (
+                        <Link key={car.id} to={`/cars/${car.id}`} className="card card-recommendation">
+                            <img src={require('../../../../images/cars/recommendation-mercedes-E220.jpg')} className="card-img-top img-fluid" alt="Car Image" />
                             <div className="card-body d-flex flex-column justify-content-between">
                                 <div>
                                     <h5 className="card-title text-white">Mercedes-Benz E 220 d AMG Line</h5>
-                                    <p className="card-text text-white">2019 • {formatPrice(189998)} km • Diesel • 194 hp</p>
+                                    <p className="card-text text-white">{car.date.split('-')[0]} • {formatPrice(car.mileage)} km • {car.fuelType} • {car.horsePower} hp</p>
                                 </div>
-                                <span className='card-price'>{formatPrice(37500)} <span>EUR</span></span>
+                                <span className='card-price'>{formatPrice(car.price)} <span>EUR</span></span>
                             </div>
-                    </Link>
-                    <Link to={`/cars/${carId}`} className="card card-recommendation">
-                        <img src={require('../../../../images/cars/recommendation-porsche-taycan-4S.jpg')} className="card-img-top img-fluid" alt="Car Image"/>
-                            <div className="card-body d-flex flex-column justify-content-between">
-                                <div>
-                                    <h5 className="card-title text-white">Porsche Taycan 4S</h5>
-                                    <p className="card-text text-white">2020 • {formatPrice(49000)} km • Electric • 530 hp</p>
-                                </div>
-                                <span className='card-price'>{formatPrice(73950)} <span>EUR</span></span>
-                            </div>
-                    </Link>
-                    <Link to={`/cars/${carId}`} className="card card-recommendation">
-                        <img src={require('../../../../images/cars/recommendation-audi-rs4.jpg')} className="card-img-top img-fluid" alt="Car Image"/>
-                            <div className="card-body d-flex flex-column justify-content-between" style={{rowGap: '20px'}}>
-                                <div>
-                                    <h5 className="card-title text-white">Audi RS4 Avant 2.9 TSI quattro Tiptronic</h5>
-                                    <p className="card-text text-white">2018 • {formatPrice(113076)} km • Gasoline • 450 hp</p>
-                                </div>
-                                <span className='card-price'>{formatPrice(85000)} <span>EUR</span></span>
-                            </div>
-                    </Link>
+                        </Link>
+                    ))}
                 </div>
             </div>
         </div>
