ea047706695a8ccc5dabaf94c2758fa39de504be
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-10-21 00:45:38 +0100

    fetch car by id

--- frontend/src/layouts/carDetails/components/CarKeyInfo/CarKeyInfo.tsx
+++ frontend/src/layouts/carDetails/components/CarKeyInfo/CarKeyInfo.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './CarKeyInfo.css'
+import CarModel from '../../../../models/CarModel';
 
 export const CarKeyInfo: React.FC<{
+    car: CarModel | undefined,
     formatValueSpaces: any
 }> = (props) => {
 
@@ -10,37 +12,37 @@ export const CarKeyInfo: React.FC<{
             <div className='d-flex flex-column justify-content-center align-items-center'>
                 <i className="bi bi-speedometer2 lh-1" style={{ color: 'white' }}></i>
                 <p className='text-white mb-2 fs-6'>Mileage</p>
-                <span className='text-white fs-5'>{props.formatValueSpaces(189998)} KM</span>
+                <span className='text-white fs-5'>{props.formatValueSpaces(props.car?.mileage)} KM</span>
             </div>
             <div className='d-flex flex-column justify-content-center align-items-center'>
                 <i className="bi bi-fuel-pump lh-1" style={{ color: 'white' }}></i>
                 <p className='text-white mb-2 fs-6'>Fuel</p>
-                <span className='text-white fs-5'>Diesel</span>
+                <span className='text-white fs-5'>{props.car?.fuelType}</span>
             </div>
             <div className='d-flex flex-column justify-content-center align-items-center'>
                 <i className="bi bi-gear lh-1" style={{ color: 'white' }}></i>
                 <p className='text-white mb-2 fs-6'>Gear Box</p>
-                <span className='text-white fs-5'>Automatic</span>
+                <span className='text-white fs-5'>{props.car?.gearBox}</span>
             </div>
             <div className='d-flex flex-column justify-content-center align-items-center'>
                 <i className="bi bi-calendar-event lh-1" style={{ color: 'white' }}></i>
                 <p className='text-white mb-2 fs-6'>Year</p>
-                <span className='text-white fs-5'>2019</span>
+                <span className='text-white fs-5'>{props.car?.date.split('-')[0]}</span>
             </div>
             <div className='d-flex flex-column justify-content-center align-items-center'>
                 <i className="bi bi-ev-front lh-1" style={{ color: 'white' }}></i>
                 <p className='text-white mb-2 fs-6'>Horse Power</p>
-                <span className='text-white fs-5'>194 HP</span>
+                <span className='text-white fs-5'>{props.car?.horsePower} HP</span>
             </div>
             <div className='d-flex flex-column justify-content-center align-items-center'>
                 <i className="bi bi-box-seam-fill lh-1" style={{ color: 'white' }}></i>
                 <p className='text-white mb-2 fs-6'>Displacement</p>
-                <span className='text-white fs-5'>{props.formatValueSpaces(1950)} cm3</span>
+                <span className='text-white fs-5'>{props.formatValueSpaces(props.car?.displacement)} cm3</span>
             </div>
             <div className='d-flex flex-column justify-content-center align-items-center'>
                 <i className="bi bi-car-front lh-1" style={{ color: 'white' }}></i>
                 <p className='text-white mb-2 fs-6'>Style</p>
-                <span className='text-white fs-5'>Coupe</span>
+                <span className='text-white fs-5'>{props.car?.style}</span>
             </div>
         </div>
     );
