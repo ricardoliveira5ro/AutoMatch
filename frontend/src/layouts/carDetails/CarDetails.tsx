a794336e5bb99f19c2cc57870392c4a1d9650eb2
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-10-21 00:46:05 +0100

    fetch car by id

--- frontend/src/layouts/carDetails/CarDetails.tsx
+++ frontend/src/layouts/carDetails/CarDetails.tsx
@@ -7,6 +7,7 @@ import { CarDescription } from './components/CarDescription/CarDescription';
 import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import CarModel from '../../models/CarModel';
+import { SpinnerLoading } from '../utils/components/SpinnerLoading/SpinnerLoading';
 
 export const CarDetails = () => {
 
@@ -75,18 +76,34 @@ export const CarDetails = () => {
 
     }, []);
 
+    if (isLoading) {
+        return (
+            <div className='mt-5'>
+                <SpinnerLoading />
+            </div>
+        )
+    }
+
     return (
         <div className='container w-100'>
             <Link className='d-flex flex-row align-items-center back-home mt-3' to='/'>
                 <i className="bi bi-house-fill" style={{ color: 'white', fontSize: '30px' }}></i>
                 <p className='d-none d-sm-flex text-white ms-3 mb-0'>Home</p>
             </Link>
-            <div className='row g-0 pt-1 pb-3'>
-                <Carousel />
-                <KeyDetails car={car} formatValueSpaces={formatValueSpaces} />
-            </div>
-            <CarKeyInfo car={car} formatValueSpaces={formatValueSpaces} />
-            <CarDescription car={car} />
+            {httpError ? (
+                <div className='mt-3'>
+                    <p className='text-white'>{httpError}</p>
+                </div>
+            ) : (
+                <>
+                    <div className='row g-0 pt-1 pb-3'>
+                        <Carousel />
+                        <KeyDetails car={car} formatValueSpaces={formatValueSpaces} />
+                    </div>
+                    <CarKeyInfo car={car} formatValueSpaces={formatValueSpaces} />
+                    <CarDescription car={car} />
+                </>
+            )}
         </div>
     );
 }
