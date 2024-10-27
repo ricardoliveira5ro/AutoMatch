80488b2b848decc81b6275de31c181f6800bb950
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-10-27 01:21:36 +0100

    Seed data cars

--- backend/src/main/java/com/ricardo/autoMatch/configuration/DataLoader.java
+++ backend/src/main/java/com/ricardo/autoMatch/configuration/DataLoader.java
@@ -2,17 +2,20 @@ package com.ricardo.autoMatch.configuration;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ricardo.autoMatch.model.User;
+import com.ricardo.autoMatch.model.*;
 import com.ricardo.autoMatch.repository.CarRepository;
 import com.ricardo.autoMatch.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.security.SecureRandom;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -57,6 +60,58 @@ public class DataLoader implements CommandLineRunner {
     }
 
     private void carSeedData() {
+        try {
+            InputStream inputStream = new ClassPathResource("static/seed/cars.json").getInputStream();
+            List<Map<String, Object>> carsData = objectMapper.readValue(inputStream, new TypeReference<>() {});
+
+            for (Map<String, Object> carData : carsData) {
+                Car car = new Car(
+                    (String) carData.get("title"),
+                    (String) carData.get("description"),
+                    (String) carData.get("make"),
+                    (String) carData.get("model"),
+                    Condition.valueOf((String) carData.get("condition")),
+                    Float.parseFloat(String.valueOf(carData.get("price"))),
+                    Style.valueOf((String) carData.get("style")),
+                    new SimpleDateFormat("yyyy-M-dd").parse((String) carData.get("date")),
+                    Integer.parseInt(String.valueOf(carData.get("mileage"))),
+                    FuelType.valueOf((String) carData.get("fuelType")),
+                    GearBox.valueOf((String) carData.get("gearBox")),
+                    Color.valueOf((String) carData.get("color")),
+                    Integer.parseInt(String.valueOf(carData.get("doors"))),
+                    Integer.parseInt(String.valueOf(carData.get("displacement"))),
+                    Integer.parseInt(String.valueOf(carData.get("horsePower")))
+                );
+
+                User user = userRepository.findById(Integer.parseInt(String.valueOf(carData.get("userId")))).orElseThrow();
+                car.setUser(user);
+
+                List<String> imagesPaths = objectMapper.convertValue(carData.get("imagesPaths"), new TypeReference<>() {});
+
+                int[] index = {1};
+                imagesPaths.forEach(path -> {
+                    try {
+                        byte[] imageData = StreamUtils.copyToByteArray(new ClassPathResource(path).getInputStream());
+
+                        if (index[0] == 1) {
+                            car.setImgCover(imageData);
+                        }
+
+                        CarImage carImage = new CarImage(imageData, index[0]++);
+                        carImage.setCar(car);
+
+                        car.getCarImages().add(carImage);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+                carRepository.save(car);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Failed to load car seed data: " + e.getMessage());
+        }
 
     }
 
