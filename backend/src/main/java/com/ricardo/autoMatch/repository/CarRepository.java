20f25528c055b1d8a79ff9d23f7d974cfe0f5026
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-11-02 13:51:44 +0000

    basic filtering search (banner filters)

--- backend/src/main/java/com/ricardo/autoMatch/repository/CarRepository.java
+++ backend/src/main/java/com/ricardo/autoMatch/repository/CarRepository.java
@@ -17,9 +17,11 @@ public interface CarRepository extends JpaRepository<Car, Long> {
             "(?1 IS NULL OR UPPER(c.make) = ?1)" +
             "AND (?2 IS NULL OR UPPER(c.model) = ?2)" +
             "AND (?3 IS NULL OR c.fuelType = ?3)" +
-            "AND (?4 IS NULL OR c.mileage = ?4)" +
-            "AND (?5 IS NULL OR c.horsePower = ?5)" +
-            "AND (?6 IS NULL OR UPPER(c.description) LIKE %?6%)"
+            "AND (?4 IS NULL OR EXTRACT(YEAR FROM c.date) = ?4)" +
+            "AND (?5 IS NULL OR c.mileage <= ?5)" +
+            "AND (?6 IS NULL OR c.price <= ?6)" +
+            "AND (?7 IS NULL OR c.horsePower >= ?7)" +
+            "AND (?8 IS NULL OR UPPER(c.description) LIKE %?8%)"
     )
-    List<Car> findFiltered(String make, String model, FuelType fuelType, Integer mileage, Integer horsePower, String searchQuery);
+    List<Car> findFiltered(String make, String model, FuelType fuelType, Integer year, Integer maxMileage, Float maxPrice, Integer minHorsePower, String searchQuery);
 }
